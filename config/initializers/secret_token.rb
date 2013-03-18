a9a340be7f12901de041dcc0bdd252fac3605ade
aoslm <aoslm12193@gmail.com>
2013-03-18 12:23:03 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prirucka::Application.config.secret_token = '35503e908a6978b04d1d96f702501cca4f30f7a94c1f6719c0923a0347373d3e17a98c067e2e10027c5f86c94fbfab3642dca263a4fd7b80d09f7e9c603802b0'
